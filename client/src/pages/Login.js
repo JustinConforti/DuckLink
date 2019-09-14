3b98e45acfb4bdcb2d342dfbef011feac9d657aa
Justin <justinconforti@gmail.com>
2019-09-14 11:30:32 -0400

    added links to homepage and registration

---
+++ client/src/pages/Login.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./style.css";
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+class Login extends Component {
+    state = {
+        email: "",
+        password: ""
+    };
+
+
+ componentDidMount() {
+    
+ }
+ handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+render() {
+    return (
+<div className="Register">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel> Email </FormLabel>{" "}
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />{" "}
+          </FormGroup>{" "}
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel> Password </FormLabel>{" "}
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>{" "}
+          <Button
+            block
+            bsSize="large"
+            type="submit"
+          >
+            register{" "}
+          </Button>{" "}
+        </form>{" "}
+      </div>
+
+    )}
+}
+
+export default Login;
