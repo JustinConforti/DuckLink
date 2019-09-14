47cd555d93592665595e2f910eb70609898e5f42
Justin <justinconforti@gmail.com>
2019-09-14 11:53:21 -0400

    added git ignore and server.js

---
+++ client/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+// const apiRoutes = require("./routes");
+const PORT = process.env.PORT || 6000;
+const app = express();
+const mongoose = require("mongoose");
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Use apiRoutes
+// app.use(apiRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
