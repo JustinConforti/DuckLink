72b475f847946df0a0f8ea08ea0cf3b006a97f91
vilagen <vilagen@yahoo.com>
2019-09-16 07:37:35 -0400

    Still working on passport.js

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,5 @@
 const db = require("../models");
-
 module.exports = {
-
-	
 
 	findById: function(req, res) {
 	  db.User
@@ -23,5 +20,5 @@ module.exports = {
 		.findOneAndUpdate({ _id: req.params.id }, req.body)
 		.then(dbModel => res.json(dbModel))
 		.catch(err => res.status(422).json(err));
-	}
+	},
 }
