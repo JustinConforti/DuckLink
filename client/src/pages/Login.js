5109e066bd13137625b4d5e66b36bdf918f2a623
Meow <IsabellaGianna29@outlook.com>
2019-09-19 16:03:37 -0400

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./style.css";
-import { Container, Col, Card, CardBody, Row } from "react-bootstrap/";
-import FormGroup from "react-bootstrap/FormGroup";
-import FormLabel from "react-bootstrap/FormLabel";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+//import { Container, Col, Card, CardBody, Row } from "react-bootstrap/";
+//import FormGroup from "react-bootstrap/FormGroup";
+//import FormLabel from "react-bootstrap/FormLabel";
+//import FormControl from "react-bootstrap/FormControl";
+//import Button from "react-bootstrap/Button";
 
 class Login extends Component {
   state = {
@@ -22,37 +22,46 @@ class Login extends Component {
 
   render() {
     return (
-      <Container>
-          <Card>
-            <Card.Body>
-              <div className="Login">
-              <img id = "logoImg" src="assets/images/duck.png" alt="..." height= "400 px" width= "400 px"></img>
-                <form onSubmit={this.handleSubmit}>
-                  <FormGroup controlId="userName" bsSize="large">
-                    <FormLabel> Username </FormLabel>{" "}
-                    <FormControl
-                      autoFocus
-                      type="username"
-                      value={this.state.username}
-                      onChange={this.handleChange}
-                    />{" "}
-                  </FormGroup>{" "}
-                  <FormGroup controlId="password" bsSize="large">
-                    <FormLabel> Password </FormLabel>{" "}
-                    <FormControl
-                      value={this.state.password}
-                      onChange={this.handleChange}
-                      type="password"
-                    />
-                  </FormGroup>{" "}
-                  <Button block bsSize="large" type="submit">
-                    Duck Duck GO{" "}
-                  </Button>{" "}
-                </form>{" "}
-              </div>
-            </Card.Body>
-          </Card>
-      </Container>
+      <div className = "container">
+            <div className = "row">
+
+                <div className="col">
+                </div>
+
+                    <div classNames="col-10">
+                        <div className="card">
+                            <div className="card-body card-body-window">
+                                    <img id = "logoImg" src="assets/images/duck.png" alt="..." height= "400 px" width= "400 px"></img>
+                                    <h1 className="display-4" id = "titleTextStyling">LogIn</h1>
+                                        <form className = "mx-auto">
+                                            <div className="form-group">
+                                                <input type="username" className="form-control" id="exampleInputuserName" aria-describedby="usernameHelp" placeholder="Enter username" />
+                                            </div>
+                                            <div className="form-group">
+                                                <input type="password" className="form-control" id="InputPassword" placeholder="Password" />
+                                            </div>
+                                            <div className="form-group">
+                                                <button type="button" className="btn btn-outline-primary">Duck Duck Go</button>
+                                            </div>
+                                        </form>
+                                  
+                            </div>
+                        </div>                               
+                    </div>
+
+                <div className="col">
+                </div>
+
+             </div>
+        </div>
+
+       ///<div className ="screen">
+      
+
+     // </div>
+    
+
+    
     );
   }
 }
