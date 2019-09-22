4ffe8738314e3f8a16369fdcd62d69fef50a2a57
Shakwan Williams <shakwanwilliamsart@gmail.com>
2019-09-22 14:25:19 -0400

    modified login.js and register.js. only modified visual JSX elements. shouldn't interfere with backend. login.js buttons need to be setup correctly for backend use.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -32,19 +32,41 @@ class Login extends Component {
                         <div className="card">
                             <div className="card-body card-body-window" style={{borderColor: " #ff899f"}}>
                                     <img id = "logoImg" src="assets/images/duck.png" alt="..." height= "400 px" width= "400 px"></img>
-                                    <h1 className="display-4" id = "titleTextStyling">LogIn</h1>
-                                        <form className = "mx-auto">
+                                    <h1 className="display-4 mx-auto" id = "titleTextStyling">LOG IN</h1>
+                                        <form className = "mx-auto fontPusher">
                                             <div className="form-group">
-                                                <input type="username" className="form-control" id="exampleInputuserName" aria-describedby="usernameHelp" placeholder="Enter username" />
+                                              <input type="username"
+                                                  name="username" 
+                                                  className="form-control" 
+                                                  id="exampleInputUsername1" 
+                                                  aria-describedby="emailHelp" 
+                                                  placeholder="Enter username"
+                                                  onChange={this.handleChange}
+                                                  value={this.state.username} />
                                             </div>
                                             <div className="form-group">
-                                                <input type="password" className="form-control" id="InputPassword" placeholder=" Enter Password" />
+                                              <input type="username"
+                                                  name="username" 
+                                                  className="form-control" 
+                                                  id="exampleInputUsername1" 
+                                                  aria-describedby="emailHelp" 
+                                                  placeholder="Enter username"
+                                                  onChange={this.handleChange}
+                                                  value={this.state.username} />
                                             </div>
+
                                             <div className="form-group">
-                                                <button type="button" className="btn btn-outline-primary" id="button1" >Duck Duck Go</button>
+                                                <button type="button" 
+                                                  className="btn btn-primary" 
+                                                  id="buttonStyle" 
+                                                  onClick={() => this.onClickSaveUser(this.state.email, this.state.username, this.state.password)}>DUCK DUCK GO!!!</button>
                                             </div>
+
                                             <div className="form-group">
-                                                <button type="button" className="btn btn-outline-primary" id="button2" >Quack?</button>
+                                                <button type="button" 
+                                                  className="btn btn-primary" 
+                                                  id="buttonStyle" 
+                                                  onClick={() => this.onClickSaveUser(this.state.email, this.state.username, this.state.password)}>REGISTER</button>
                                             </div>
                                         </form>
                                   
