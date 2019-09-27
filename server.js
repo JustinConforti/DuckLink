fdcf3084d3026103e8bef12f719851959016ea84
Justin <justinconforti@gmail.com>
2019-09-26 20:26:43 -0400

    server.js changes to try and deploy

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Ducks", { useNe
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, 'build')));
+// }
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(session({
   secret: 'secret',
@@ -54,8 +55,8 @@ function connectToMongo() {
 
 // Send every request to the React app
 // Define any API routes before this runs
- app.get("*", function(req, res) {
-   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+ app.get("/*", function(req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
 
